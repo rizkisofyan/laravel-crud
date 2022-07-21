32173af6dc018a1cb1dc8158e872ee1a91c0fd04
rzkfyn <rzkfyn@gmail.com>
2022-07-21 20:43:19 +0700

    Latihan templating

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,12 +47,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        @auth
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('siswa.index') }}">Siswa</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('pembelian.index') }}">Pembelian</a>
                         </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
